8d3efd2e4c37db86143c94807033e105c778bd32
codecrazeoverbaes05 <dubeyanushma@gmail.com>
2018-03-09 23:40:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function (req, res) {
-     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two',function (req, res) {
-    res.send('Article two is being served, WILL BE THERE');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three',function (req, res) {
