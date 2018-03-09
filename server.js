e65a964066611b546d8852e0e900588caaec2f20
codecrazeoverbaes05 <dubeyanushma@gmail.com>
2018-03-09 23:56:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/article-two',function (req, res) {
 });
 
 app.get('/article-three',function (req, res) {
-    res.send('Article three is being served, WILL BE THERE');
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
